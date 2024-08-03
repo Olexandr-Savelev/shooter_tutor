8b61e889e33779df8c366085d30c2043e9cc5887
Oleksandr Savelev <a.savelev.krm@gmail.com>
2024-08-03 19:35:30 +0300

    add inside level(first stage)
    Signed-off-by: Oleksandr Savelev <a.savelev.krm@gmail.com>

--- scenes/levels/outside.gd
+++ scenes/levels/outside.gd
@@ -3,3 +3,12 @@ extends RootLevel
 func _on_gate_player_entered(_body):
 	var tween = create_tween()
 	tween.tween_property(player, 'speed', 0, 0.1)
+
+func _on_house_player_entered():
+	var tween = get_tree().create_tween()
+	tween.tween_property(player.get_node('Camera2D'), 'zoom', Vector2(0.8,0.8), 1)
+
+
+func _on_house_player_left():
+	var tween = get_tree().create_tween()
+	tween.tween_property(player.get_node('Camera2D'), 'zoom', Vector2(0.5,0.5), 1)
