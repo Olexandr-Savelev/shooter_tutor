6bbc64ed60a09ab832af0f9dbd6f3278c0b457be
Oleksandr Savelev <a.savelev.krm@gmail.com>
2024-08-06 02:13:30 +0300

    add colors for projectiles ui

--- scenes/levels/inside.gd
+++ scenes/levels/inside.gd
@@ -5,3 +5,4 @@ func _on_area_2d_body_entered(_body):
 	var tween = create_tween()
 	tween.tween_property(player, 'speed', 0, 0.5)
 	get_tree().change_scene_to_file("res://scenes/levels/outside.tscn")
+
--- scenes/ui/ui.gd
+++ scenes/ui/ui.gd
@@ -1,10 +1,33 @@
 extends CanvasLayer
 
+var green: Color = Color("43b691")
+var red: Color = Color("dd210e")
+
 @onready var laser_label = $LaserCounter/VBoxContainer/Label
 @onready var grenade_label = $GrenadeCounter/VBoxContainer/Label
+@onready var laser_icon = $LaserCounter/VBoxContainer/TextureRect
+@onready var grenade_icon = $GrenadeCounter/VBoxContainer/TextureRect
+
+
+func _ready():
+	update_laser_count()
+	update_grenade_count()
 
 func update_laser_count():
-	laser_label.text = str(Globals.laser_amount)
+	var laser_amount = Globals.laser_amount
+	laser_label.text = str(laser_amount)
+	update_color(laser_amount, laser_label, laser_icon)
 
 func update_grenade_count():
-	grenade_label.text = str(Globals.grenade_amount)
+	var grenade_amount = Globals.grenade_amount
+	grenade_label.text = str(grenade_amount)
+	update_color(grenade_amount, grenade_label, grenade_icon)
+
+func update_color(amount, label, icon):
+	if amount > 0:
+		label.modulate = green
+		icon.modulate = green
+	else:
+		label.modulate = red
+		icon.modulate = red
+		
